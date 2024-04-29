a028aa70f185c40c3779cc1915d0d8a8824b9655
devia <devia@192.168.1.34>
2024-04-29 10:55:34 +0530

    xml files

--- SeleniumProject1/src/test/java/base/BaseClass.java
+++ SeleniumProject1/src/test/java/base/BaseClass.java
@@ -1,12 +1,15 @@
 package base;
 import utilities.CaptureScreenshotOnFailure;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
@@ -21,16 +24,23 @@ public class BaseClass {
 		prop.load(ip);
 	}
 
-	@BeforeMethod
-	public void beforeMethod() throws IOException {
+	@BeforeMethod(alwaysRun = true)
+	@Parameters("browser")
+	public void beforeMethod(String name) throws IOException {
 		testBasic();
-		driver = new ChromeDriver();
+		if (name.equals("chrome")) {
+			driver = new ChromeDriver();
+		} else if (name.equals("edge")) {
+			driver = new EdgeDriver();
+
+		}
 		driver.get(prop.getProperty("baseUrl"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
 	}
 
-	@AfterMethod
+
+	@AfterMethod(alwaysRun = true)
 
 	public void afterMethod(ITestResult iTestResult) throws IOException {
 		if (iTestResult.getStatus() == ITestResult.FAILURE) {
@@ -39,6 +49,7 @@ public class BaseClass {
 		}
 
 		driver.close();
+
 	}
 
 }
